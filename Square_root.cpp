e54b9dfbebc5cbb2bb94081ed70c015f9f0fb50b
imcoder23 <deependraparajuli@gmail.com>
2019-05-20 20:22:17 -0700

    Square root

---
+++ Square_root.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+
+    if (x == 0 || x == 1) 
+    return x; 
+  
+    long long int i = 1; 
+    long long int  result = 1; 
+    while (result <= x) 
+    { 
+      i++; 
+      result = i * i; 
+    } 
+    return i - 1; 
+        
+    }
+};
+
